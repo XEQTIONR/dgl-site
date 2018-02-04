523b238968c43c12e28cb9c0dcf00020f43468a0
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2018-02-04 10:49:19 -0500

    Can create a basic tournament.
    Validation rules not in place. Title FK not in place

---
+++ app/Http/Controllers/TournamentController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tournament;
+use Illuminate\Http\Request;
+
+class TournamentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('tournaments.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        //dd("HEREY");
+        $tournament = new Tournament;
+
+        $tournament->name = $request->name;
+        $tournament->description = $request->description;
+        $tournament->rules = $request->rules;
+        $tournament->startdate = $request->startdate;
+        $tournament->enddate = $request->enddate;
+
+        $tournament->save();
+
+        return redirect('/');
+        //echo "STORE FUNCTION";
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Tournament  $tournament
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tournament $tournament)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Tournament  $tournament
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tournament $tournament)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Tournament  $tournament
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tournament $tournament)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Tournament  $tournament
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tournament $tournament)
+    {
+        //
+    }
+}
