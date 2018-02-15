1d5d45561bb316c7b8aed27f7734224fd00ebb64
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2018-02-14 22:23:58 -0500

    game query by id, email. or alias

--- app/Http/Controllers/GamerController.php
+++ app/Http/Controllers/GamerController.php
@@ -43,19 +43,25 @@ class GamerController extends Controller
      * @param  alias or email  $identifier
      * @return \Illuminate\Http\Response
      */
-    public function show($identifier)
+    public function show($identifier, Request $request)
     {
         //
+      //dd($request->route()->uri);
         $is_email = filter_var($identifier, FILTER_VALIDATE_EMAIL);
 
-        if($is_email)
+        if($is_email) //identifier is an email try searching by email
           $gamer = Gamer::where('email', $identifier)->first();
         else
-          $gamer = Gamer::where('alias', $identifier)->first();
-
+        {
+          //try searching by id
+          $gamer=Gamer::where('id', $identifier)->first();
+          if(is_null($gamer)) // if fails try searching by alias
+            $gamer = Gamer::where('alias', $identifier)->first();
+        }
         if(is_null($gamer))
           return "NOT FOUND";
 
+        $gamer->uri = $request->route()->uri;
         return $gamer;
     }
 
