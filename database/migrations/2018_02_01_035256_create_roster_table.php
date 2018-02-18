20cb1a1507eb0de7c709c4b32ab42e0831fb25c1
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2018-02-18 17:31:46 -0500

    Adding 'confirmation_required' option on 'status' field of roster table

--- database/migrations/2018_02_01_035256_create_roster_table.php
+++ database/migrations/2018_02_01_035256_create_roster_table.php
@@ -27,7 +27,7 @@ class CreateRosterTable extends Migration
 
             $table->unsignedInteger('contending_team_id');
             $table->unsignedInteger('gamer_id');
-            $table->enum('status',['ineligible','platform_id_required','email_verification_required','ok']);
+            $table->enum('status',['ineligible','confirmation_required','platform_id_required','email_verification_required','ok']);
             $table->foreign('contending_team_id')->references('id')->on('contending_teams');
             $table->foreign('gamer_id')->references('id')->on('gamers');
             $table->timestamps();
