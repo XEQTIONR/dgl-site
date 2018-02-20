c7d6ed4bb8d0a30767f86fb2913f53c7f457f4dc
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2018-02-19 20:31:19 -0500

    BUGFIX : verification link was not being generated in email properly

--- resources/views/mail/verify_email_address.blade.php
+++ resources/views/mail/verify_email_address.blade.php
@@ -1,3 +1,3 @@
 DGLCORE.COM <br>
 Click the following link to verify your email address : {{$emailAddress}} <br>
-<a href="localhost:8000/verify/{{$verificationCode}}">THIS LINK</a>
+<a href="http://localhost:8000/verify/{{$verificationCode}}">THIS LINK</a>
