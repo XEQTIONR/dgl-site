6d57d5d72348d280f2e263cd63b281253d8abdeb
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2018-02-25 16:56:25 -0500

    Created migration for 'match_contestant' table

---
+++ database/migrations/2018_02_25_214730_create_match_contestant_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMatchContestantTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('match_contestant', function (Blueprint $table) {
+
+            $table->unsignedInteger('match_id');
+            $table->unsignedInteger('contending_team_id');
+            $table->timestamps();
+
+            $table->primary(['match_id', 'contending_team_id']);
+            $table->foreign('match_id')->references('id')->on('matches');
+            $table->foreign('contending_team_id')->references('id')->on('contending_teams');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('match_contestant');
+    }
+}
