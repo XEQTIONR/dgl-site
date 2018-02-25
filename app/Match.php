17fb7e8d6da7a73536d001f410fb6b1fbb67f175
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2018-02-25 16:29:17 -0500

    Adding eloquent relationships for Match model

--- app/Match.php
+++ app/Match.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Match extends Model
 {
-    //
+  //
+  protected $table = 'matches';
+
+  public function tournament()
+  {
+    return $this->belongsTo('App\Tournament');
+  }
+
+  public function winner()
+  {
+    // won_id references id on contending_teams table
+    return $this->belongsTo('App\ContendingTeam', 'won_id');
+  }
 }
--- app/Tournament.php
+++ app/Tournament.php
@@ -10,11 +10,16 @@ class Tournament extends Model
 
     public function esport()
     {
-        return $this->belongsTo('App\Esport','title'); // FK title references id
+      return $this->belongsTo('App\Esport','title'); // FK title references id
     }
 
     public function contenders()
     {
       return $this->hasMany('App\ContendingTeam');
+    }
+
+    public function matches()
+    {
+      return $this->hasMany('App\Match');
     }
 }
