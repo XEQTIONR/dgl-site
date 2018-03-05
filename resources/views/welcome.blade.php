92632a2a086a6339d6b2c5a27ebcfe7b6d484bdc
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2018-03-04 22:45:11 -0500

    CSS changes and fixing the logout form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,44 +5,60 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>DaGameLeague - DGLcore.com</title>
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
+        <!--Fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Titillium+Web:400,400i,700,700i" rel="stylesheet">
+        <!--Stylesheet-->
         <link rel="stylesheet" href="/css/app.css">
-        {{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">--}}
-
+        <!--JQuery-->
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-
-        {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>--}}
-        {{--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>--}}
 
     </head>
     <body>
-        <div class="container">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="row">
-                <div class="col-md-12">
-                    Laravel
-                </div>
-
-                <div class="col-md-1 col-md-offset-9">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                Professional Esports Leagues and Tournaments
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-10" style="border: 1px solid black;">
+                    DAGAMELEAGUE
                 </div>
             </div>
-        </div>
+            <div class="row main-menu">
+                <div class="col-1"></div>
+                <div class="col">
+                    <ul class="nav justify-content-start">
+                        <li class="nav-item"><a class="nav-link">news</a></li>
+                        <li class="nav-item"><a class="nav-link">tournaments</a></li>
+                        <li class="nav-item"><a class="nav-link">matches</a></li>
+                        <li class="nav-item"><a class="nav-link">teams</a></li>
+                        <li class="nav-item"><a class="nav-link">players</a></li>
+                        <li class="nav-item"><a class="nav-link">media</a></li>
+                        <li class="nav-item"><a class="nav-link">about us</a></li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <ul class="nav justify-content-end">
+                        @if(Auth::guest())
+                            <li class="nav-item"><a href="{{  route('login')  }}" class="nav-link">sign in</a></li>
+                            <li class="nav-item"><a href="{{  route('register')  }}" class="nav-link">register</a></li>
+                        @else
+                            <li class="nav-item">
+                              <a href="{{ route('logout') }}" class="nav-link"
+                              onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                              logout
+                              </a>
+                            </li>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                              {{ csrf_field() }}
+                            </form>
+                        @endif
+                    </ul>
+                </div>
+                <div class="col-1"></div>
+            </div>
+            
         {{--<script src="/js/popper.min.js"></script>--}}
         {{--<script src="/js/bootstrap.min.js"></script>--}}
         <script src="/js/app.js"></script>
