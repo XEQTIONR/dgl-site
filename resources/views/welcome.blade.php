8e26879b37e835e9daf62417c0c86b3d9854246c
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2018-03-05 00:27:30 -0500

    Adding a default image carousel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,7 +27,7 @@
             </div>
             <div class="row main-menu">
                 <div class="col-1"></div>
-                <div class="col">
+                <div class="col-8">
                     <ul class="nav justify-content-start">
                         <li class="nav-item"><a class="nav-link" href="/blog">news</a></li>
                         <li class="nav-item"><a class="nav-link" href="/tournaments">tournaments</a></li>
@@ -69,10 +69,39 @@
                     </ul>
                 </div>
                 <div class="col-1"></div>
+            </div> <!-- row main-menu -->
+            <div class="row">
+                <div class="col-10 offset-1">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100" src="{{URL::asset('storage/1.jpg')}}" alt="First slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="{{URL::asset('storage/2.jpg')}}" alt="Second slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="{{URL::asset('storage/3.jpg')}}" alt="Third slide">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+                </div>
             </div>
 
-        {{--<script src="/js/popper.min.js"></script>--}}
-        {{--<script src="/js/bootstrap.min.js"></script>--}}
-        <script src="/js/app.js"></script>
+            <script src="/js/app.js"></script>
+        </div> <!--container-fluid-->
     </body>
 </html>
