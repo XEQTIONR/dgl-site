ac9db3d300b53348a21dbc46f25dc5b24e11546b
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2018-03-05 23:20:36 -0500

    Basic main page scaffolding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,23 +70,43 @@
                 </div>
                 <div class="col-1"></div>
             </div> <!-- row main-menu -->
-            <div class="row">
-                <div class="col-10 offset-1">
+            <div class="row banner-background">
+                <div class="col-lg-10 offset-lg-1">
                 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
                     <ol class="carousel-indicators">
                         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to=""></li>
                     </ol>
                     <div class="carousel-inner">
                         <div class="carousel-item active">
                             <img class="d-block w-100" src="{{URL::asset('storage/1.jpg')}}" alt="First slide">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>DGL Faceoff 5</h5>
+                                <p>DOTA 2 tournament</p>
+                            </div>
                         </div>
                         <div class="carousel-item">
                             <img class="d-block w-100" src="{{URL::asset('storage/2.jpg')}}" alt="Second slide">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>DA* League</h5>
+                                <p>1st division DOTA2 League</p>
+                            </div>
                         </div>
                         <div class="carousel-item">
                             <img class="d-block w-100" src="{{URL::asset('storage/3.jpg')}}" alt="Third slide">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Overpowered</h5>
+                                <p>Overwatch tournament</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="{{URL::asset('storage/2.jpg')}}" alt="Fouth slide">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>DA* League</h5>
+                                <p>1st division DOTA2 League</p>
+                            </div>
                         </div>
                     </div>
                     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -99,7 +119,66 @@
                     </a>
                 </div>
                 </div>
+            </div> <!-- row banner-background -->
+            <div class="row justify-content-center main">
+                <div class="col-md-8">
+                    <div class="row main-content">
+                        <div class="col-12">
+                            <div class="row main-content-heading">
+                                <div class="col-12">
+                                    <h2>LATEST</h2>
+                                </div>
+                            </div>
+                            <div class="row main-content-posts">
+                                <div class="col-12">
+                                    <div class="row post-body" style="margin-top : 10px">
+                                        <div class="col-4">
+                                            <img class="img-fluid" src="{{URL::asset('storage/2.jpg')}}">
+                                        </div>
+                                        <div class="col-8">
+                                            <h3>The title of the post</h3>
+                                            <p>The excerpt of the post</p>
+                                        </div>
+                                    </div>
+                                    <div class="row post-body" style="margin-top : 10px">
+                                        <div class="col-4">
+                                            <img class="img-fluid" src="{{URL::asset('storage/1.jpg')}}">
+                                        </div>
+                                        <div class="col-8">
+                                            <h3>The title2 of the post</h3>
+                                            <p>The excerpt2 of the post</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="row  justify-content-center main-sidebar">
+                        <div class="col-12" style="background-color: #00b3ee">
+                            <h5>Some widget stuff</h5>
+                        </div>
+                    </div>
+                </div>
+            </div><!-- row main -->
+            <div class="row justify-content-center footer">
+                <div class="col-md-7">
+                    <div class="row footer-text">
+                        <div class="col-12">
+                            <h2>DAGAMELEAGUE</h2>
+                            <h2>DGLcore.com</h2>
+                            <p>All rights reserved</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="row footer-img">
+
+                    </div>
+                </div>
             </div>
+
 
             <script src="/js/app.js"></script>
         </div> <!--container-fluid-->
