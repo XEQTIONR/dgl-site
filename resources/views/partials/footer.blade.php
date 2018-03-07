fd8f12f08df96b86a08dc1af36caa761347071fe
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2018-03-06 23:31:53 -0500

    Moved footer to a partial file

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,16 @@
+<div class="row justify-content-center footer">
+  <div class="col-md-7">
+    <div class="row footer-text">
+      <div class="col-12">
+        <h2>DAGAMELEAGUE</h2>
+        <h2>DGLcore.com</h2>
+        <p>All rights reserved</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-3">
+    <div class="row footer-img">
+
+    </div>
+  </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -127,23 +127,8 @@
                     </div>
                 </div>
             </div><!-- row main -->
-            <div class="row justify-content-center footer">
-                <div class="col-md-7">
-                    <div class="row footer-text">
-                        <div class="col-12">
-                            <h2>DAGAMELEAGUE</h2>
-                            <h2>DGLcore.com</h2>
-                            <p>All rights reserved</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="row footer-img">
 
-                    </div>
-                </div>
-            </div>
-
+            @include('partials.footer')
 
             <script src="/js/app.js"></script>
         </div> <!--container-fluid-->
