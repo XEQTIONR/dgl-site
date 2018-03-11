dda43485eebbb6f01830e975b2e1c871a7491b9b
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2018-03-10 22:01:05 -0400

    Replacing the main menu with bootstrap responsice main menu + custom css

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -10,62 +10,88 @@
   @author XEQTIONR
 
 --}}
-<div class="row justify-content-center top-banner">
-  Professional Esports Leagues and Tournaments
+<div class="row justify-content-center top-banner d-none d-lg-block">
+ <h6 class="text-center font-primary-700b">Professional Esports Leagues and Tournaments</h6>
 </div>
+
 <div class="row justify-content-center align-items-end no-gutters">
-  <div class="col-2 col-sm-1 offset-sm-1 col-md-1 offset-md-1" style="">
+  <div class="col offset-sm-1 offset-md-1 d-none d-lg-block" style="">
     <div class="title-banner-img-container">
-      <img class="title-banner-img" src="{{URL::asset('storage/DGLCrownPrimary.svg')}}" />
+      <img class="title-banner-img" style="vertical-align: baseline " src="{{URL::asset('storage/DGLCrownPrimary.svg')}}" />
     </div>
   </div>
-  <div class="col">
-      <h3 class="regular">ĐAGAMELEAGUE</h3>
+  <div class="col-10 mb-4 d-none d-xl-block">
+      <h2 class="font-primary-700bi">ĐAGAMELEAGUE</h2>
+  </div>
+  <div class="col-10 mb-3 d-none d-lg-block d-xl-none">
+    <h2 class="font-primary-700bi">ĐAGAMELEAGUE</h2>
+  </div>
+  <div class="col-12 d-lg-none">
+    <div class="row justify-content-center" id="alertBannerTop">
+      <div class="col-3 col-sm-2 my-3">
+        <img class="title-banner-img" style="vertical-align: baseline " src="{{URL::asset('storage/DGLCrownGray.svg')}}" />
+      </div>
+    </div>
   </div>
 </div>
-<div class="row main-menu">
-  <div class="col-1">
-  </div>
-  <div class="col-8">
-    <ul class="nav justify-content-start">
-      <li class="nav-item"><a class="nav-link" href="/blog">news</a></li>
-      <li class="nav-item"><a class="nav-link" href="/tournaments">tournaments</a></li>
-      <li class="nav-item"><a class="nav-link" href="/matches">matches</a></li>
-      <li class="nav-item"><a class="nav-link" href="/teams">teams</a></li>
-      <li class="nav-item"><a class="nav-link" href="/players">players</a></li>
-      <li class="nav-item"><a class="nav-link" href="/media">media</a></li>
-      <li class="nav-item"><a class="nav-link" href="/about">about us</a></li>
-    </ul>
-  </div>
-  <div class="col">
-    <ul class="nav justify-content-end">
-      @if(Auth::guest())
-        <li class="nav-item">
-          <a href="{{  route('login')  }}" class="nav-link">
-            <i class="fas fa-sign-in-alt"></i>
-            sign in
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="{{  route('register')  }}" class="nav-link">
-            <i class="fas fa-user-plus"></i>
-            register
-          </a>
-        </li>
-      @else
-        <li class="nav-item">
+<div class="row justify-content-center navbar-dgl-background">
+  <div class="col-lg-10">
+    <nav class="navbar navbar-expand-md navbar-dgl">
+      <button class="navbar-toggler lightgray-border" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a href="/news" class="nav-link" href="#">news<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item dropdown">
+            <a href="/tournaments" class="nav-link" href="#">tournaments</a>
+          </li>
+          <li class="nav-item">
+            <a href="/matches" class="nav-link" href="#">matches</a>
+          </li>
+          <li class="nav-item">
+            <a href="/teams" class="nav-link" href="#">teams</a>
+          </li>
+          <li class="nav-item">
+            <a href="/players" class="nav-link" href="#">players</a>
+          </li>
+          <li class="nav-item">
+            <a href="/media" class="nav-link" href="#">media</a>
+          </li>
+          <li class="nav-item">
+            <a href="/about" class="nav-link" href="#">about us</a>
+          </li>
+        </ul>
+        @if(Auth::guest())
+          <span class="">
+            <a href="{{  route('login')  }}" class="nav-link">
+              <i class="fas fa-sign-in-alt"></i>
+              sign in
+            </a>
+          </span>
+          <span class="">
+            <a href="{{  route('register')  }}" class="nav-link">
+              <i class="fas fa-user-plus"></i>
+              register
+            </a>
+          </span>
+        @else
+          <span>
           <a href="{{ route('logout') }}" class="nav-link"
              onclick="event.preventDefault();
                                 document.getElementById('logout-form').submit();">
             <i class="fas fa-sign-out-alt"></i>
             logout
           </a>
-        </li>
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-          {{ csrf_field() }}
-        </form>
-      @endif
-    </ul>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              {{ csrf_field() }}
+            </form>
+          </span>
+        @endif
+      </div>
+    </nav>
   </div>
-  <div class="col-1"></div>
-</div> <!-- row main-menu -->
+</div>
