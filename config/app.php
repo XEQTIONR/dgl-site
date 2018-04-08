fd8f538ad6f14ee353236541c7035f4b78b43f74
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2018-04-08 11:39:31 -0400

    Composer requiring intervention/image for image processing/saving
    composer require intervention/image
    + Basic setup.

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -225,6 +226,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Image' => Intervention\Image\Facades\Image::class
 
     ],
 
