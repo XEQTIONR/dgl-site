b0b8fb0e180780e50d96629b4549c1c4ee7c1127
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2018-04-15 13:47:20 -0400

    Created checkin table migration

---
+++ database/migrations/2018_04_15_172943_create_checkins_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCheckinsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checkins', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('match_id');
+            $table->unsignedInteger('contending_team_id');
+            $table->unsignedInteger('gamer_id');
+            $table->timestamps();
+
+            $table->foreign(['match_id', 'contending_team_id'])->references(['match_id', 'contending_team_id'])->on('match_contestants');
+            $table->foreign(['gamer_id', 'contending_team_id'])->references(['gamer_id', 'contending_team_id'])->on('rosters');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checkins');
+    }
+}
