11c0a520cfe51e236e6faee4ce1f7f95dfcf5c74
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2018-06-24 12:02:46 -0400

    Adding subject to RegisterforTournament

--- app/Mail/RegisterforTournament.php
+++ app/Mail/RegisterforTournament.php
@@ -3,6 +3,8 @@
 namespace App\Mail;
 
 use App\ContendingTeam;
+use App\Gamer;
+use App\Roster;
 use App\Tournament;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
@@ -21,6 +23,7 @@ class RegisterforTournament extends Mailable
     public $alias;
     public $team;
     public $tournament;
+    public $captain;
 
     public function __construct($alias, ContendingTeam $team, Tournament $tournament)
     {
@@ -28,6 +31,9 @@ class RegisterforTournament extends Mailable
         $this->alias = $alias;
         $this->team = $team;
         $this->tournament = $tournament;
+        $roster = Roster::where('contending_team_id',$team->id)
+                        ->where('captain', 'true')->first();
+        $this->captain = Gamer::find($roster->gamer_id);
     }
 
     /**
@@ -37,6 +43,8 @@ class RegisterforTournament extends Mailable
      */
     public function build()
     {
-        return $this->view('mail.register_confirmation_for_tournament');
+        return $this->from('admin@dglcore.com')
+                    ->subject($this->captain->alias.' ('.$this->captain->fname.' '.$this->captain->lname.') invited you to participate in tournament : '.$this->tournament->name.' for his team '.$this->team->name.'.')
+                    ->view('mail.register_confirmation_for_tournament');
     }
 }
