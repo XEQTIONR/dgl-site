9a817c46ecf8cfa373575aff87ac6cb64a6570a5
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2018-06-25 15:02:22 -0400

    Adding Steam and Battnet verification in website sign up form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,22 +1,139 @@
 @extends('layouts.simple')
 @section('header-scripts')
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker3.standalone.min.css">
+    <script src="http://code.jquery.com/jquery-3.2.1.min.js" ></script>
     <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+
 @endsection
 @section('footer-scripts')
-    <script src="http://code.jquery.com/jquery-3.2.1.min.js" ></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker3.standalone.min.css">
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/js/bootstrap-datepicker.js"></script>
     <script>
         $('#dob').datepicker({
             format: 'dd/mm/yyyy',
             autoclose: true,
-            startDate: '-90y',
-            endDate: '-12y'
+
+            // startDate: '-90y',
+            // endDate: '-12y'
         });
 
         $('#dob-btn').click(function(){
             $('#dob').focus();
         })
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.5.13/dist/vue.js"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    <script src="/js/app.js"></script>
+    <style>
+      .visible{
+        /**visibility : visible;*/
+        display: inherit;
+      }
+      .visible-inline{
+        display: inline;
+      }
+      .hidden{
+        /**visibility: hidden;*/
+        display: none;
+      }
+    </style>
+    <script>
+        let data = {
+            searchingSteam: false,
+            steamIdInput: "",
+            steamProfileFound: false,
+            steamPersonaName: "",
+            steamAvatarURL: "",
+
+            searchingBattleTag: false,
+            battleTagInput: "",
+            owProfileFound: false,
+            owAvatarURL: "",
+
+            submit_disabled: false
+        };
+
+        var app = new Vue({
+           el: '#app',
+           data: data,
+           methods:{
+
+               submitDisable: function(){
+                   app.submit_disabled = true;
+               },
+               submitEnable: function () {
+                   if(!app.searchingBattleTag || !app.searchingSteam)
+                       app.submit_disabled = false;
+               },
+
+               getSteamInfo: function()//steam64id
+               {
+                   var statuses = ['offline','online','busy','away','snooze','looking to trade', 'looking to play'];
+                   if(app.steamIdInput!="" && !app.searchingSteam) {
+                       app.searchingSteam = true;
+                       axios.get('/steamapi/' + this.steamIdInput).then(function (response) {
+                           var steam_profile = response.data;
+
+                           app.searchingSteam = false;
+                           app.submit_disabled = false;
+                           toastr.options = {
+                               "closeButton" : true,
+                               "timeOut": "5000",
+                           };
+
+                           if (steam_profile.responseStatus == 'success')
+                           {
+
+                               app.steamProfileFound = true;
+                               // app.steamid = "" + app.steamIdInput;
+                               app.steamPersonaName = steam_profile.personaname;
+                               app.steamAvatarURL = steam_profile.avatarmedium;
+                               // app.steamStatus = statuses[steam_profile.personastate];
+                               toastr.success("Steam profile found.");
+                           }
+                           else
+                           {
+                               app.steamProfileFound = false;
+                               app.steamIdInput="";
+                               toastr.error("Could not find your profile. :(");
+                           }
+                       });
+                   }
+               },
+               getOverwatchInfo: function()
+               {
+                   if(app.battleTagInput!="" && !app.searchingBattleTag) {
+                       app.searchingBattleTag = true;
+                       app.submit_disabled = false;
+                       axios.get('/owapi/' + this.battleTagInput.replace("#", "-")).then(function (response) {
+                           var ow_data = response.data;
+
+                           app.searchingBattleTag = false;
+
+
+                           toastr.options = {
+                               "closeButton" : true,
+                               "timeOut": "5000",
+                           };
+
+                           if (ow_data.responseStatus == 'success')
+                           {
+                               app.owProfileFound = true;
+                               app.owAvatarURL = ow_data.data;
+                               toastr.success("BattleNet profile found.");
+
+                           }
+                           else{
+                               console.log('response status: '+ ow_data.responseStatus);
+                               app.owProfileFound = false;
+                               app.battleTagInput="";
+                               toastr.error("Could not find your profile. :(");
+                           }
+                       });
+                   }
+               }
+           }
+        });
     </script>
 @endsection
 @section('content')
@@ -28,8 +145,8 @@
             </div>
             <div class="row justify-content-center"><h6>Sign up for DaGameLeague</h6></div>
             <div class="row mt-4 justify-content-center px-1 px-md-5">
-                <div class="col-12">
-                    <form class="form-horizontal w-100" method="POST" action="{{ route('register') }}">
+                <div id="app" class="col-12">
+                    <form id="" class="form-horizontal w-100" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('fname') ? ' has-error' : '' }}">
@@ -88,11 +205,15 @@
                             </div>
                         </div>
 
-                        <div class="input-group col date mb-3 {{ $errors->has('dob') ? ' has-error' : '' }}">
+                        <div class="form-group mb-3">
                             <label for="dob" class="col-md-4 control-label">Birthday</label>
 
-                            {{--<div class="col">--}}
-                                <input id="dob" name="dob" class="form-control" value="{{ old('dob') }}" required autofocus autocomplete="off">
+                            <div id="dob" class="input-group date px-3"
+                                 data-provide="datepicker"
+                                 data-date-format="dd/mm/yyyy"
+                                 data-date-end-date="-10y"
+                                 data-date-start-date="01/01/1950">
+                                <input name="dob" class="form-control" required autofocus autocomplete="off">
                                 <div id="dob-btn" class="input-group-addon px-3 py-2" style="display: inline; background-color: #67c; border-radius: 2px">
                                     <i class="far fa-calendar-alt" style="color: #FFF"></i>
                                 </div>
@@ -101,35 +222,78 @@
                                         <strong>{{ $errors->first('dob') }}</strong>
                                     </span>
                                 @endif
-                            {{--</div>--}}
+                            </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('fname') ? ' has-error' : '' }}">
+                        <div v-if="!steamProfileFound" class="form-group">
                             {{--<label for="steamid" class="col-md-4 control-label">Steam ID:</label>--}}
 
                             <div class="col">
-                                <input id="steamid" type="text" class="form-control" name="steamid" value="{{ old('steamid') }}" placeholder="SteamID"  autofocus>
-
-                                @if ($errors->has('steamid'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('steamid') }}</strong>
-                                    </span>
-                                @endif
+                              <div class="input-group">
+                                <input v-model="steamIdInput" v-on:change="getSteamInfo()" id="steamid" type="text" class="form-control" value="{{ old('steamid') }}" placeholder="SteamID"  autofocus>
+                                <label v-show="!searchingSteam" class="input-group-addon btn mb-0 bg-lightestgray" v-on:click="getSteamInfo()" for="date">
+                                  <span class="fas fa-search"></span>
+                                  {{--<span v-bind:class="[{'fa-spin fas fa-circle-notch': !searchingBattleTag },{'fas fa-search': searchingBattleTag}]"></span>--}}
+                                </label>
+                                <label v-show="searchingSteam" class="input-group-addon btn mb-0 bg-lightestgray" for="date">
+                                  <span class="fa-spin fas fa-circle-notch"></span>
+                                  {{--<span v-bind:class="[{'fa-spin fas fa-circle-notch': !searchingBattleTag },{'fas fa-search': searchingBattleTag}]"></span>--}}
+                                </label>
+                              </div>
                             </div>
                         </div>
+                        <div v-else class="form-group row justify-content-center mx-2 p-2" style="border: 2px solid #6677cc;">
+                          <div class="col-12">
+                            <label>Steam64ID</label>
+                          </div>
+                          <div class="col">
+                            <img v-bind:src="steamAvatarURL">
+                          </div>
+                          <div class="col">
+                            <h3>@{{ steamPersonaName }}</h3>
+                            {{--<span>@{{ steamStatus }}</span>--}}
 
-                        <div class="form-group{{ $errors->has('battlenetid') ? ' has-error' : '' }}">
+                            {{-- hidden inputs for those which get replaced in DOM--}}
+                            <input name="steamid" v-model="steamIdInput" type="hidden">
+                            <input name="steamAvatarURL" v-model="steamAvatarURL" type="hidden">
+
+                          </div>
+                          <div class="col">
+                            <button v-on:click="steamProfileFound = false; steamIdInput = '';" type="button" class="btn btn-danger d-block ml-auto mr-0">X</button>
+                          </div>
+                        </div>
+
+                        <div v-if="!owProfileFound" class="form-group">
                             {{--<label for="fname" class="col-md-4 control-label">Battle.Net ID:</label>--}}
-
                             <div class="col">
-                                <input id="battlenetid" type="text" class="form-control" name="battlenetid" value="{{ old('battlenetid') }}"  placeholder="BattleNet Account"  autofocus>
-
-                                @if ($errors->has('battlenetid'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('battlenetid') }}</strong>
-                                    </span>
-                                @endif
+                              <div class="input-group">
+                                <input v-model="battleTagInput" v-on:change="getOverwatchInfo()" id="battlenetid" type="text" class="form-control"  value="{{ old('battlenetid') }}"  placeholder="BattleNet Account"  autofocus>
+                                <label v-show="!searchingBattleTag" class="input-group-addon btn mb-0 bg-lightestgray" v-on:click="getOverwatchInfo()" for="date">
+                                  <span class="fas fa-search"></span>
+                                  {{--<span v-bind:class="[{'fa-spin fas fa-circle-notch': !searchingBattleTag },{'fas fa-search': searchingBattleTag}]"></span>--}}
+                                </label>
+                                <label v-show="searchingBattleTag" class="input-group-addon btn mb-0 bg-lightestgray" for="date">
+                                  <span class="fa-spin fas fa-circle-notch"></span>
+                                  {{--<span v-bind:class="[{'fa-spin fas fa-circle-notch': !searchingBattleTag },{'fas fa-search': searchingBattleTag}]"></span>--}}
+                                </label>
+                              </div>
                             </div>
+                        </div>
+                        <div v-else class="form-group row justify-content-center mx-2 p-2" style="border: 2px solid #6677cc;">
+                          <div class="col-12">
+                            <label>Battle Tag</label>
+                          </div>
+                          <div class="col">
+                            <img v-bind:src="owAvatarURL" width="64" height="64">
+                          </div>
+                          <div class="col">
+                            <h3>@{{ battleTagInput }}</h3>
+                            <input name="battlenetid"  v-model="battleTagInput" type="hidden">
+                            <input name="battleNetAvatarURL" v-model="owAvatarURL" type="hidden">
+                          </div>
+                          <div class="col">
+                            <button v-on:click="owProfileFound = false; battleTagInput = '';" type="button" class="btn btn-danger d-block ml-auto mr-0">X</button>
+                          </div>
                         </div>
 
                         <div class="form-group{{ $errors->has('discordid') ? ' has-error' : '' }}">
