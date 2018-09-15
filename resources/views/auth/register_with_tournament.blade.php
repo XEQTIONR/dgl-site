b716b85e36d14ac67594aa06e16be4b4da96aa0d
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2018-09-14 22:59:00 -0400

    Change steam id label in registration forms

--- resources/views/auth/register_with_tournament.blade.php
+++ resources/views/auth/register_with_tournament.blade.php
@@ -322,7 +322,7 @@
 
                 <div class="col">
                   <div class="input-group">
-                    <input v-model="steamIdInput" v-on:change="getSteamInfo()" id="steamid" name="steamid" type="text" class="form-control" value="{{ old('steamid') }}" placeholder="SteamID"  autofocus>
+                    <input v-model="steamIdInput" v-on:change="getSteamInfo()" id="steamid" name="steamid" type="text" class="form-control" value="{{ old('steamid') }}" placeholder="Steam64ID"  autofocus>
                     <label v-show="!searchingSteam" class="input-group-addon btn mb-0 bg-lightestgray" v-on:click="getSteamInfo()" for="date">
                       <span class="fas fa-search"></span>
                       {{--<span v-bind:class="[{'fa-spin fas fa-circle-notch': !searchingBattleTag },{'fas fa-search': searchingBattleTag}]"></span>--}}
