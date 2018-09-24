7be7a2dc307ae618f6018714cb33bdf6b6e1e161
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2018-09-23 20:22:38 -0400

    Adding twitch icon/link

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -16,9 +16,10 @@
 
 </div>
 <div class="row footer justify-content-center">
-  <a class="mx-2 mb-5" href="https://discord.gg/{{config('social.discord_invite_code')}}"> <img src="{{URL::asset('storage/icons8-discord.png')}}" width="30" height="30"> </a>
-  <a class="mx-2 mb-5" href="https://facebook.com/dglcore"> <img src="{{URL::asset('storage/icons8-facebook.png')}}" width="30" height="30"> </a>
-  <a class="mx-2 mb-5" href="mailto:admin@dglcore.com"> <img src="{{URL::asset('storage/icons8-mail.png')}}" width="30" height="30"> </a>
+  <a class="mx-2 mb-5" href="https://twitch.tv/dagameleague"> <img src="{{URL::asset('storage/icons8-twitch-filled-50.png')}}" width="30" height="30" alt="DaGameLeague Twitch Chanel"> </a>
+  <a class="mx-2 mb-5" href="https://discord.gg/{{config('social.discord_invite_code')}}"> <img src="{{URL::asset('storage/icons8-discord.png')}}" width="30" height="30" alt="DaGameLeague Discord Chanel"> </a>
+  <a class="mx-2 mb-5" href="https://facebook.com/dglcore"> <img src="{{URL::asset('storage/icons8-facebook.png')}}" width="30" height="30" alt="DaGameLeague Facebook page"> </a>
+  <a class="mx-2 mb-5" href="mailto:admin@dglcore.com"> <img src="{{URL::asset('storage/icons8-mail.png')}}" width="30" height="30"  alt="DaGameLeague Admin Email"> </a>
 </div>
 <div class="row footer bg-purple5">
   <div class="col ml-5 my-3 mw footer-text">
